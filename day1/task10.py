eed3d1fb14777a3cde3e67dc005c54aa1a2e93c8
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-06-20 15:23:44 +0400

    Create task10.py

---
+++ day1/task10.py
@@ -0,0 +1,11 @@
+import math
+
+a = int(input("Please , input the first number: ")
+b = int(input("Please , input the second number: ")
+print(f'Sum is {a+b}')
+print(f'Difference is {a-b}')
+print(f'Quotient is {a/b}')
+print(f'Total quotient is {int(a/b)}')
+print(f'Division balance is {a%b}')
+print(f'Natural log of a is {math.log(a)}')
+        
