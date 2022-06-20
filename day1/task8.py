cae53efbd9fbeba7da2d096268c39c1ec3e2070f
hovomuradyan <88653435+hovomuradyan@users.noreply.github.com>
2022-06-20 15:08:58 +0400

    Create task8.py

---
+++ day1/task8.py
@@ -0,0 +1,5 @@
+souvenir = int(input("Please , input your souvenirs count: "))
+other = int(input("Please , input count of other items: "))
+weight_of_souvenir = 75 * souvenir
+weight_of_other_items = 112 * other
+print("Total weight" , weight_of_souvenir + weight_of_other_items)
